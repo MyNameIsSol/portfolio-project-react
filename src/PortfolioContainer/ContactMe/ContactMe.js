548c4f7612aa1aed79363840bf1570b137adeefc
MyNameIsSol <kclark6612@gmail.com>
2025-05-15 16:12:20 +0100

    edit src and save

---
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ContactMe.css';
+
+export default function ContactMe() {
+    return(
+        <div className="contact-me-container">
+            <div className="contact-me-parent">
+                <div className="contact-me-title">
+                    <h1></h1>
+                </div>
+            </div>
+        </div>
+
+    )
+}
